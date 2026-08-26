def5a9cdfe15f626579ed8d4bf1e7eb6b9448c37
Ankit Pandit <ankitkumarpandit1221@gmail.com>
2026-08-26 11:44:38 +0530

    feat: section routes are done

--- apps/backend/src/section.ts
+++ apps/backend/src/section.ts
@@ -2,6 +2,7 @@ import express from "express"
 import { prisma } from "db/client"
 import { authMiddleware } from "../middlewares/authMiddleware"
 import { createSectionSchema, updateBoardSchema } from "../schema"
+import { checkAdminRole } from "../utils/checkAdminRole"
 
 const router = express.Router()
 
@@ -28,6 +29,15 @@ router.post("/section", authMiddleware, async (req, res) => {
     })
   }
 
+  const userId = req.id
+  
+  if(!(await checkAdminRole(userId, board.orgId))) {
+    return res.status(403).json({
+      success: false,
+      error: "forbidden, you're have to access to modify this resource"
+    })
+  } 
+  
   const section = await prisma.section.create({
     data: {
       title: data.title,
@@ -36,82 +46,59 @@ router.post("/section", authMiddleware, async (req, res) => {
   })
 
   return res.status(201).json({
-    success: false,
+    success: true,
     message: "section created successfully",
     data: section
   })
   
 })
 
-router.get("/section/:sectionId", authMiddleware, async (req, res) => {
-  const sectionId = req.params.sectionId as string
-
-  if (!sectionId) {
+router.get("/sections", authMiddleware, async (req, res) => {
+  const orgId = req.query.orgId as string
+  const userId = req.id
+  
+  if (!orgId) {
     return res.status(400).json({
       success: false,
-      error: "sectionId is required"
+      error: "orgId is required"
     })
   }
 
-  const board = await prisma.board.findUnique({
+  const membership = await prisma.membership.findFirst({
     where: {
-      id: sectionId
+      userId: userId,
+      orgId: orgId
     }
   })
 
-  if (!board) {
-    return res.status(400).json({
+  if (!membership) {
+    return res.status(403).json({
       success: false,
-      error: "board not found"
-    })
-  }
-
-  const section = await prisma.section.findUnique({
-    where: {
-      id: sectionId
-    }
-  })
-
-  if (!section) {
-    return res.status(400).json({
-      success: false,
-      error: "section not found"
+      error: "forbidden, you're have to access to read this resource"
     })
   }
   
-  return res.status(200).json({
-    success: true,
-    message: "section found successfully",
-    data: section
-  })
-})
-
-router.get("/sections", authMiddleware, async (req, res) => {
-  const sectionId = req.query.sectionId as string
-
-  if (!sectionId) {
-    return res.status(400).json({
-      success: false,
-      error: "sectionId is required"
-    })
-  }
-
-  const section = await prisma.section.findUnique({
+  const boards = await prisma.board.findMany({
     where: {
-      id: sectionId
+      id: orgId 
     },
     include: {
-      board: true
+      section: true
     }
   })
 
-  if (!section) {
+  if (!boards) {
     return res.status(400).json({
       success: false,
       error: "section not found"
     })
   }
 
+  return res.status(200).json({
+    success: true,
+    message: "fetched all sections successfully",
+    data: boards
+  })
   
 })
 
@@ -126,6 +113,7 @@ router.patch("/section/:sectionId", authMiddleware, async (req, res) => {
   }
 
   const sectionId = req.params.sectionId as string
+  const userId = req.id
 
   if (!sectionId) {
     return res.status(400).json({
@@ -133,8 +121,8 @@ router.patch("/section/:sectionId", authMiddleware, async (req, res) => {
       error: "sectionId is required"
     })
   }
-
-  const section = await prisma.section.findUnique({
+  
+  const existingSection = await prisma.section.findUnique({
     where: {
       id: sectionId
     },
@@ -143,13 +131,20 @@ router.patch("/section/:sectionId", authMiddleware, async (req, res) => {
     }
   })
 
-  if (!section) {
+  if (!existingSection) {
     return res.status(400).json({
       success: false,
       error: "section not found"
     })
   }
-   
+  
+  if(!(await checkAdminRole(userId, existingSection.board.orgId))) {
+    return res.status(403).json({
+      success: false,
+      error: "forbidden, you're have to access to modify this resource"
+    })
+  }
+  
   const updatedSection = await prisma.section.update({
     where: {
       id: sectionId
@@ -163,6 +158,52 @@ router.patch("/section/:sectionId", authMiddleware, async (req, res) => {
     success: true,
     message: "section updated successfully",
     data: updatedSection
+  })
+})
+
+router.delete("/section/:sectionId", authMiddleware, async (req, res) => {
+  const sectionId = req.params.sectionId as string
+  const userId = req.id
+
+  if (!sectionId) {
+    return res.status(400).json({
+      success: false,
+      error: "sectionId is required"
+    })
+  }
+
+  const section =  await prisma.section.findUnique({
+    where: {
+      id: sectionId
+    },
+    include: {
+      board: true
+    }
+  })
+
+  if (!section) {
+    return res.status(404).json({
+      success: false,
+      error: "section not found"
+    })
+  }
+
+  if(!(await checkAdminRole(userId, section.board.orgId))) {
+    return res.status(403).json({
+      success: false,
+      error: "forbidden, you're have to access to modify this resource"
+    })
+  }
+
+  await prisma.section.delete({
+    where: {
+      id: sectionId
+    }
+  })
+
+  return res.status(200).json({
+    success: true,
+    message: "section deleted successfully"
   })
 })
 
